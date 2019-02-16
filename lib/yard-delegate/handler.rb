9cbadaa81422fad67a2bc8683bb2a383d7616688
jmchambers <jonathan.m.chambers@gmail.com>
2019-02-16 18:55:14 +0000

    Added rescue block around instance_eval, as it can lead to uncaught errors when running "yard gems"

--- lib/yard-delegate/handler.rb
+++ lib/yard-delegate/handler.rb
@@ -1,28 +1,33 @@
+# frozen_string_literal: true
+
 require 'yard'
 
 module YARD
   module Delegate
     class Handler < YARD::Handlers::Ruby::Base
-
       handles method_call(:delegate)
 
       def delegate(*names)
         hash = names.pop
-        to   = hash[:to] || hash["to"]
+        to   = hash[:to] || hash['to']
 
         names.each do |name|
           register YARD::CodeObjects::MethodObject.new(namespace, name, scope) { |o|
-            o.parameters = [["*args", nil], ["&block", nil]]
+            o.parameters = [['*args', nil], ['&block', nil]]
             o.source = "def #{name}(*args, &block)\n  #{to}.#{name}(*args, &block)\nend"
             o.signature = "def #{name}(*args, &block)"
-            o.docstring = statement.comments.to_s.empty? ? "" : statement.comments
+            o.docstring = statement.comments.to_s.empty? ? '' : statement.comments
             o.delegate = to
           }
         end
       end
 
       process do
-        instance_eval(statement.source, __FILE__, __LINE__)
+        begin
+          instance_eval(statement.source, __FILE__, __LINE__)
+        rescue StandardError
+          nil
+        end
       end
     end
   end
